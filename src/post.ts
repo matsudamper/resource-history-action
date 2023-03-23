1f74d584f5e961e630183017fafc22ab393b408f
matsudamper <let@matsudamper.net>
2023-03-23 18:24:57 +0900

    awaiot

--- src/post.ts
+++ src/post.ts
@@ -12,18 +12,20 @@ const CPU_FILE = "/var/tmp/action-history-cpu.txt";
 // exec("kill " + process.env.CPU_PID);
 
 const cpuLines: any[] = fs.readFileSync(CPU_FILE, 'utf8').split("\n");
-cpuLines.unshift({ data: 'CPU', header: true });
+cpuLines.unshift({data: 'CPU', header: true});
 
 const memoryLines: any[] = fs.readFileSync(MEMORY_FILE, 'utf8').split("\n");
-memoryLines.unshift({ data: 'Memory', header: true });
+memoryLines.unshift({data: 'Memory', header: true});
 
 console.log("cpuLines=" + cpuLines);
 console.log("memoryLines=" + memoryLines);
 
-core.summary
-    .addHeading('Resource Usage')
-    .addTable([
-        cpuLines,
-        memoryLines
-    ])
-    .write();
+(async () =>
+        await core.summary
+            .addHeading('Resource Usage')
+            .addTable([
+                cpuLines,
+                memoryLines
+            ])
+            .write()
+)()
