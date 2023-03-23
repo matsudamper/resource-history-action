c49a3b8d89080cc04ac3a70c841c9baa6d11fe83
matsudamper <matsudamper971227@gmail.com>
2023-03-23 17:00:40 +0900

    remove main...

--- pre.js
+++ pre.js
@@ -5,9 +5,11 @@ const fs = require('fs');
 const MEMORY_FILE = "/var/tmp/action-history-memory.txt"
 const CPU_FILE = "/var/tmp/action-history-cpu.txt"
 
+console.log("===================1")
 fs.writeFileSync(MEMORY_FILE, '')
 fs.writeFileSync(CPU_FILE, '')
 
+console.log("===================2")
 const options = {
     detached: true,
     stdio: ['ignore'],
@@ -16,8 +18,12 @@ const options = {
 const cpuProcess = spawn(path.join(__dirname, 'scripts', 'cpu.sh'), options);
 const memoryProcess = spawn(path.join(__dirname, 'scripts', 'memory.sh'), options);
 
+console.log("===================3")
 fs.appendFileSync(process.env.GITHUB_ENV, 'MEMORY_PID=' + memoryProcess.pid + "\n")
 fs.appendFileSync(process.env.GITHUB_ENV, 'CPU_PID=' + cpuProcess.pid + "\n")
 
+
+console.log("===================4")
 cpuProcess.unref();
 memoryProcess.unref();
+console.log("===================5")
