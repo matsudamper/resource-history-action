bca395f60d0811a31fa2a67f2daf86a1024da6ba
matsudamper <let@matsudamper.net>
2023-03-23 18:14:43 +0900

    typescriptに置き換え

---
+++ dist/pre/index.js
@@ -0,0 +1,130 @@
+module.exports =
+/******/ (function(modules, runtime) { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete installedModules[moduleId];
+/******/ 		}
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	__webpack_require__.ab = __dirname + "/";
+/******/
+/******/ 	// the startup function
+/******/ 	function startup() {
+/******/ 		// Load entry module and return exports
+/******/ 		return __webpack_require__(159);
+/******/ 	};
+/******/
+/******/ 	// run startup
+/******/ 	return startup();
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 129:
+/***/ (function(module) {
+
+module.exports = require("child_process");
+
+/***/ }),
+
+/***/ 159:
+/***/ (function(__unusedmodule, exports, __webpack_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const path = __importStar(__webpack_require__(622));
+const child_process_1 = __webpack_require__(129);
+const fs = __importStar(__webpack_require__(747));
+const MEMORY_FILE = "/var/tmp/action-history-memory.txt";
+const CPU_FILE = "/var/tmp/action-history-cpu.txt";
+console.log("===================1");
+fs.writeFileSync(MEMORY_FILE, '');
+fs.writeFileSync(CPU_FILE, '');
+console.log("===================2");
+const options = {
+    detached: true,
+    stdio: ['ignore'],
+};
+const cpuProcess = (0, child_process_1.spawn)("nohup", [path.join(__dirname, 'scripts', 'cpu.sh', '&')], {
+    env: Object.assign(process.env, {
+        RUNNER_TRACKING_ID: ""
+    })
+});
+const memoryProcess = (0, child_process_1.spawn)("nohup", [path.join(__dirname, 'scripts', 'memory.sh', '&')], {
+    env: Object.assign(process.env, {
+        RUNNER_TRACKING_ID: ""
+    })
+});
+console.log("===================3");
+
+
+/***/ }),
+
+/***/ 622:
+/***/ (function(module) {
+
+module.exports = require("path");
+
+/***/ }),
+
+/***/ 747:
+/***/ (function(module) {
+
+module.exports = require("fs");
+
+/***/ })
+
+/******/ });
---
+++ src/post.ts
@@ -0,0 +1,29 @@
+import * as core from "@actions/core";
+import * as fs from 'fs';
+
+for (const key in process.env) {
+    console.log(`${key}=${process.env[key]}`);
+}
+
+const MEMORY_FILE = "/var/tmp/action-history-memory.txt";
+const CPU_FILE = "/var/tmp/action-history-cpu.txt";
+
+// exec("kill " + process.env.MEMORY_PID);
+// exec("kill " + process.env.CPU_PID);
+
+const cpuLines: any[] = fs.readFileSync(CPU_FILE, 'utf8').split("\n");
+cpuLines.unshift({ data: 'CPU', header: true });
+
+const memoryLines: any[] = fs.readFileSync(MEMORY_FILE, 'utf8').split("\n");
+memoryLines.unshift({ data: 'Memory', header: true });
+
+console.log("cpuLines=" + cpuLines);
+console.log("memoryLines=" + memoryLines);
+
+core.summary
+    .addHeading('Resource Usage')
+    .addTable([
+        cpuLines,
+        memoryLines
+    ])
+    .write();
