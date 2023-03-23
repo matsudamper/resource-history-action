de8c5070197d5cb1b9a7741ff851b1762894c948
matsudamper <matsudamper971227@gmail.com>
2023-03-23 17:31:22 +0900

    tmp

--- post.js
+++ post.js
@@ -1,4 +1,4 @@
-import * as core from '@actions/core' 
+const core = require('@actions/core');
 const github = require('@actions/github');
 const { spawn, exec  } = require('child_process');
 const fs = require('fs');
@@ -19,6 +19,9 @@ cpuLines.unshift({data: 'CPU', header: true});
 
 const memoryLines = fs.readFileSync(MEMORY_FILE, 'utf8').split("\n");
 memoryLines.unshift({data: 'Memory', header: true});
+
+console.log("cpuLines=" + cpuLines);
+console.log("memoryLines=" + memoryLines);
 
 await core.summary
   .addHeading('Resource Usage')
