b45e3b698625e8aabf0482abe875620737b312bb
matsudamper <let@matsudamper.net>
2023-03-23 20:30:20 +0900

    tmp

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import * as core from '@actions/core';
+import * as path from 'path';
+import {exec, spawn, SpawnOptionsWithoutStdio} from 'child_process';
+import * as fs from 'fs';
+
+const intervalSeconds: number = parseInt(core.getInput("interval_seconds"), 10)
+console.log("intervalSeconds=" + intervalSeconds)
+
+const MEMORY_FILE = "/var/tmp/action-history-memory.txt";
+const CPU_FILE = "/var/tmp/action-history-cpu.txt";
+
+console.log("===================1");
+fs.writeFileSync(MEMORY_FILE, '');
+fs.writeFileSync(CPU_FILE, '');
+
+console.log("===================2");
+process.env.RUNNER_TRACKING_ID = "0"
+const options: SpawnOptionsWithoutStdio = {
+    // detached: true,
+    stdio: [null, null, null],
+    env: {
+        ...process.env,
+        RUNNER_TRACKING_ID: "0"
+    }
+};
+
+const cpuProcess = spawn(
+    'sh', ['-c', `RUNNER_TRACKING_ID=0 nohup ${path.join(__dirname, 'cpu.sh')} &`], options);
+const memoryProcess = spawn(
+    'sh', ['-c', `RUNNER_TRACKING_ID=0 nohup ${path.join(__dirname, 'memory.sh')} &`], options);
+
+// cpuProcess.unref()
+// memoryProcess.unref()
+console.log("===================3");
